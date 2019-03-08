a2d1f6a871bf3fac34fc146001aa63718f1547b1
Bgag2536 <47954738+Bgag2536@users.noreply.github.com>
2019-03-07 23:39:06 -0600

    Add files via upload

---
+++ actions.js
@@ -0,0 +1,26 @@
+window.onload = init();
+
+function init(){
+
+}
+
+function runPython(){
+    $(document).ready(function(){
+        $('#openOne').click(function(){
+            var a = new XMLHttpRequest();
+            a.open("GET", "OpeningRackOne");
+            a.onreadystatechange=function(){
+                    if (a.readyState == 4){
+                        if (a.status == 200){
+                            alert("SUCCESS")
+                        } 
+                        else if (a.status == 404){
+                            alert("you fucked up")
+                        }
+                        else alert("HTTP ERROR")
+                    }
+            }
+            a.send();
+        });
+    });
+}
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template, jsonify  
+from flask_restful import Resource, Api
+import Action
+from Action import openRackOne
+from Action import openRackTwo
+import PiMotor
+import time
+
+app = Flask(__name__) # Runs app
+
+@app.route('/') # base website 
+def unlockRack():
+    return render_template('EDDSite.html') #calls html
+
+@app.route('/OpeningRackOne') # opens site not generated yet but sends command and opens rack
+def openingRackOne():
+   return openRackOne() # defined in action.py with code to activate motors
+
+@app.route('/OpeningRackTwo')
+def OpeningRackTwo():
+   return openRackOne()
+
+
