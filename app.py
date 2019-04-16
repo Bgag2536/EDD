50c8ca73c08c7a05dc827b4f82ece5cd87865098
Bgag2536 <47954738+Bgag2536@users.noreply.github.com>
2019-04-16 08:13:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
-from flask import *
+                            #import all neccesary items
+rom flask import *
 import Action
 import ActionClose
 from Action import openRackOne
 from Action import openRackTwo
-from ActionClose import closeRackOne
+from ActionClose import closeRackOne    
 from ActionClose import closeRackTwo
 import PiMotor
 import time
 
 app = Flask(__name__) # Runs app
 
-@app.route('/') # base website 
+@app.route('/') #home of website 
 def unlockRack():
     return render_template('EDDSite.html') #calls html
 
-@app.route('/openRackOne.html')
+@app.route('/openRackOne.html') #creates site url
 def openingRackOne():
-   Action.openRackOne()
+   Action.openRackOne() #calls the opening function
    print('success')
-   return render_template('openRackOne.html') 
+   return render_template('openRackOne.html') #calls html site
 
 @app.route('/openRackTwo.html')
 def openingRackTwo():
@@ -39,4 +40,4 @@ def closingRackTwo():
     return render_template('closingRackTwo.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)  #sets the website address to 0.0.0.0
