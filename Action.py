a368e79eb0fd7174b5c89a5b3b3260f7dfb88568
Bgag2536 <47954738+Bgag2536@users.noreply.github.com>
2019-04-25 08:26:48 -0500

    Update Action.py

--- Action.py
+++ Action.py
@@ -1,38 +1,31 @@
 import PiMotor
 import time
 
-
 def openRackOne():
     m1 = PiMotor.Motor("MOTOR1",1) # defines motor
-
     x=1
     try:
         while (x==1):
             print("it worked")
-
-            x = x-1
-                            # code to control motors for rack one
+            x = x-1       
+            # code to control motors for rack one
             m1.forward(50)
             time.sleep(.5)
             m1.stop
-
     except:
         print('you goofed up')
-        
+      
 def openRackTwo():
     m2 = PiMotor.Motor("MOTOR2",1) # defines motor
-
     x=1
     try:
         while (x==1):
             print("it worked")
-
             x = x-1
-                            # code to control motors for rack one           
+            # code to control motors for rack one           
             m2.forward(50)
             time.sleep(.5)
             m2.stop
-
     except:
         print('you goofed up')        
 
